5bb31e617976d5953c11da61d5a6d2c35cd0dc84
Rumiya <56404235+Rumiya-dev@users.noreply.github.com>
2023-10-06 16:41:53 -0930

    customers and drivers registration in firebase

--- app/src/main/java/com/example/taxiapp/CustomerRegLoginActivity.java
+++ app/src/main/java/com/example/taxiapp/CustomerRegLoginActivity.java
@@ -1,12 +1,20 @@
 package com.example.taxiapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CustomerRegLoginActivity extends AppCompatActivity {
 
@@ -14,10 +22,20 @@ public class CustomerRegLoginActivity extends AppCompatActivity {
     Button signInBtn, signUpBtn;
     EditText emailET, passwordET;
 
+
+    FirebaseAuth mAuth;
+
+    ProgressDialog loadingBar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_reg_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        loadingBar = new ProgressDialog(this);
 
         customerStatus = (TextView)findViewById(R.id.statusCustomer);
         question = (TextView)findViewById(R.id.accountCreateCustomer);
@@ -29,6 +47,8 @@ public class CustomerRegLoginActivity extends AppCompatActivity {
         signUpBtn.setVisibility(View.INVISIBLE);//button signIn don`t visible
         signUpBtn.setEnabled(false);//can`t press the button signIn occasionally when it not visible
 
+
+
         question.setOnClickListener(new View.OnClickListener() {//knopka sign in ne vidna poka ne narzat na nadpis "you don`t have account
             @Override
             public void onClick(View view) {
@@ -37,6 +57,37 @@ public class CustomerRegLoginActivity extends AppCompatActivity {
                 signUpBtn.setVisibility(View.VISIBLE);
                 signUpBtn.setEnabled(true);
                 customerStatus.setText("Registration for the customers");//izmenenie texta nadpisi "log in for drivers"
+            }
+        });
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailET.getText().toString();//preobrazovanie vvedennogo email v strochku string
+                String password = passwordET.getText().toString();//preobrazovanie vvedennogo parolya v strochku string
+
+                RegisterCustomer(email, password);
+
+
+            }
+        });
+    }
+
+    private void RegisterCustomer(String email, String password) {
+
+        loadingBar.setTitle("User registration");
+        loadingBar.setMessage("Please wait loading");
+        loadingBar.show();
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(CustomerRegLoginActivity.this, "Registration completed successfully", Toast.LENGTH_SHORT).show();
+                    loadingBar.dismiss();
+                } else {
+                    Toast.makeText(CustomerRegLoginActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                    loadingBar.dismiss();
+                }
             }
         });
     }
--- app/src/main/java/com/example/taxiapp/DriverRegLoginActivity.java
+++ app/src/main/java/com/example/taxiapp/DriverRegLoginActivity.java
@@ -1,19 +1,37 @@
 package com.example.taxiapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class DriverRegLoginActivity extends AppCompatActivity {
      TextView driverStatus, question;//objavlenje vseh widgetov
      Button signInBtn, signUpBtn;
      EditText emailET, passwordET;
+
+     FirebaseAuth mAuth;
+
+     ProgressDialog loadingBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
+
+        loadingBar = new ProgressDialog(this);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_reg_login);//podkluchenje widjetov k id
         driverStatus = (TextView)findViewById(R.id.statusDriver);
@@ -37,7 +55,34 @@ public class DriverRegLoginActivity extends AppCompatActivity {
             }
         });
 
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailET.getText().toString();//preobrazovanie vvedennogo email v strochku string
+                String password = passwordET.getText().toString();//preobrazovanie vvedennogo parolya v strochku string
+
+                RegisterDriver(email, password);
 
 
+            }
+        });
+    }
+
+    private void RegisterDriver(String email, String password){
+        loadingBar.setTitle("User registration");
+        loadingBar.setMessage("Please wait loading");
+        loadingBar.show();
+      mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+          @Override
+          public void onComplete(@NonNull Task<AuthResult> task) {
+              if (task.isSuccessful()) {
+                  Toast.makeText(DriverRegLoginActivity.this, "Registration completed successfully", Toast.LENGTH_SHORT).show();
+                  loadingBar.dismiss();
+              } else {
+                  Toast.makeText(DriverRegLoginActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                  loadingBar.dismiss();
+              }
+          }
+      });
     }
 }
